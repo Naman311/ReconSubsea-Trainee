90c8c763b0a8ac546d44e33f14b6b5b113c3e7c5
AnkitJha1998 <37044020+AnkitJha1998@users.noreply.github.com>
2018-03-04 21:32:30 +0530

    Uploaded Basic Code

---
+++ HC-SR04/Ultrasonic_AnkitJha.ino
@@ -0,0 +1,28 @@
+int trigPin=9;
+int echoPin = 8;
+
+long duration;
+double distance;
+
+void setup() {
+  pinMode(trigPin,OUTPUT);  //Define Pin
+
+  pinMode(echoPin,INPUT);
+
+  Serial.begin(9600);
+     
+}
+
+void loop() {
+  
+    digitalWrite(trigPin,LOW);
+    delay(100);
+    digitalWrite(trigPin,HIGH);  // Create a 10 micro second pulse
+    delayMicroseconds(10);
+    digitalWrite(trigPin,LOW);
+    duration=pulseIn(echoPin,HIGH);
+    distance=duration*0.034/2;
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+}
