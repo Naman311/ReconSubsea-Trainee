91e77c5c8e13ee3a3cd094d5aff902428e30116d
harshcap1 <37062930+harshcap1@users.noreply.github.com>
2018-03-08 12:49:34 +0530

    Add files via upload

---
+++ HC-SR04/ping_harsh/sensor_ping/sensor_ping.ino
@@ -0,0 +1,40 @@
+int cycle,dist;
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(7,OUTPUT);
+  pinMode(8,INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() 
+{
+  //READING WAVE  
+  digitalWrite(7,LOW);
+  delayMicroseconds(10);
+  digitalWrite(7,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(7,LOW);
+  delayMicroseconds(10);
+  
+
+  cycle = pulseIn(8,HIGH);
+  // Serial.println("cycle ");
+   //Serial.println(cycle);
+    //Serial.println("\n");
+  dist = int(0.017*cycle);
+  Serial.println(dist);
+  //delay(250);
+
+  if(dist>=15)
+  {
+   digitalWrite(LED_BUILTIN,HIGH);
+  }
+  else
+  {
+     digitalWrite(LED_BUILTIN,LOW);
+  }
+  
+  
+}
