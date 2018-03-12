b0444783cc37496489b0201d932af94fe6c71951
satyamambast <36452790+satyamambast@users.noreply.github.com>
2018-03-13 00:04:56 +0530

    Ping_Satyam
    Documentation and Code for Arduino Project #1

---
+++ HC-SR04/ping_satyam/sketch_mar04a/sketch_mar04a.ino
@@ -0,0 +1,31 @@
+long duration,distance;
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(4, OUTPUT);
+pinMode(12, INPUT);
+Serial.begin(9600); 
+
+}
+
+void loop() {
+digitalWrite(4, LOW); //Output to pin 4: 0
+delayMicroseconds(2);
+digitalWrite(4, HIGH); //Output to pin4: 5V 
+delayMicroseconds(10);
+digitalWrite(4, LOW); //Output to pin 4: 0
+duration = pulseIn(12, HIGH); //Input from pin 12
+distance= duration*0.034/2;
+Serial.println("Distance: ");
+Serial.println(distance);
+
+if(distance<15)
+{
+  Serial.print("Condition Fulfilled");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delayMicroseconds(2000);
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  
+}
+delayMicroseconds(10);
+}
